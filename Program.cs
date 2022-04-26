e744069d72d2266f32143b422fb3883d7ef58af2
Alexanicoleee <alexandranicolecajes@gmail.com>
2022-04-26 10:09:44 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Recruiter.Data;
+using Recruiter.Models;
+using Recruiter.Repositories;
+using System;
+
+namespace Recruiter
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ConfigurationHelper configurationHelper = ConfigurationHelper.Instance();
+            var dbConnectionString = configurationHelper.GetProperty<string>("DbConnectionString");
+
+            using (RecruitmentContext context = new RecruitmentContext(dbConnectionString))
+            {
+                EmployeeRepository employeeRepository = new EmployeeRepository(context);
+                Console.Write("Enter Employee Code:");
+                string code = Console.ReadLine();
+                Employee existingEmployee = new Employee();
+                try
+                {
+                    existingEmployee = employeeRepository.FindByCode(code);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    System.Environment.Exit(1);
+                }
+                Employee position = employeeRepository.EmployeePosition(existingEmployee);
+                Console.WriteLine($"Employee Code: {code}");
+                Console.WriteLine($"First Name: {existingEmployee.VFirstName}");
+                Console.WriteLine($"Last Name: {existingEmployee.VLastName}");
+                Console.WriteLine($"Birth date: {existingEmployee.DBirthDate}");
+                Console.WriteLine($"Current Position: {existingEmployee.CCurrentPosition}");
+                employeeRepository.EmployeeSkills(code);
+                employeeRepository.Annualfees(code);
+                employeeRepository.MonthlySalary(code);
+            }
+
+        }
+    }
+}
---
+++ Repositories/EmployeeRepository.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Recruiter.Data;
+using Recruiter.Models;
+
+namespace Recruiter.Repositories
+{
+    internal class EmployeeRepository
+    {
+        public RecruitmentContext Context { get; set; }
+
+        public EmployeeRepository(RecruitmentContext context)
+        {
+            this.Context = context;
+        }
+
+        public Employee FindByCode(string employeeCode)
+        {
+            var employee = this.Context.Employees
+                .Where(e => e.CEmployeeCode.Equals(employeeCode))
+                .FirstOrDefault();
+                if (employee != null)
+                {
+                    return employee;
+                }
+
+            throw new Exception($"Employee with Employee code {employeeCode} doesn't exist");
+        }
+
+        public Employee EmployeePosition(Employee employee)
+        {
+            this.FindByCode(employee.CEmployeeCode);
+            string code = employee.CCurrentPosition.ToString();
+            var position = this.Context.Positions.Where(e => e.CPositionCode.Equals(code)).FirstOrDefault();
+            if(position != null)
+            {
+                employee.CCurrentPosition = position.VDescription;
+                return employee;
+            }
+            throw new Exception($"Employee with Current Position code {code} does'nt exist");
+        }
+
+        public void EmployeeSkills(string employeecode)
+        {
+            var skills = this.Context.EmployeeSkills
+                .Join(Context.Skills,
+                es => es.CSkillCode,
+                s => s.CSkillCode,
+                (es, s) => new
+                {
+                    EmployeeCode = es.CEmployeeCode,
+                    SkillCode = es.CSkillCode,
+                    Skill = s.VSkill
+
+                })
+                .Where(e => e.EmployeeCode.Equals(employeecode))
+                .ToList();
+
+            Console.WriteLine("***Skills***");
+            foreach(var skill in skills)
+            {
+                Console.WriteLine(skill.Skill);
+            }
+        }
+
+        public void Annualfees(string employeeCode)
+        {
+            var annualS = this.Context.AnnualSalaries
+                .Where(e => e.CEmployeeCode.Equals(employeeCode))
+                .ToList();
+
+
+            Console.WriteLine("**Annual Salary Received**");
+            foreach (var annual in annualS)
+            {
+                Console.WriteLine($"Annual Salary: {annual.MAnnualSalary}, Year: {annual.SiYear}");
+            }
+        }
+
+        public void MonthlySalary(string employeeCode)
+        {
+            var monthlySalaries = this.Context.MonthlySalaries
+                .Where(e => e.CEmployeeCode.Equals(employeeCode))
+                .ToList();
+
+
+            Console.WriteLine("**Monthly Salary Received**");
+            foreach (var monthlySalary in monthlySalaries)
+            {
+                Console.WriteLine($"Monthly Salary: {monthlySalary.MMonthlySalary}, Pay Date: {monthlySalary.DPayDate}, Referral Bonus: {monthlySalary.MReferralBonus}");
+            }
+        }
+
+    }
+}
